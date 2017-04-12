df7d187e06f06c7d66a089aaf1cd5ad2ce7cfec1
Alnordh <alnordh@kth.se>
2017-04-12 12:50:16 +0200

    ändrat en del

--- AL-AN-Tilda/09/lab9.py
+++ AL-AN-Tilda/09/lab9.py
@@ -3,31 +3,55 @@ import sys
 
 q = LinkedQ()
 
+ATOMER = ["H", "He", "Li", "Be", "B", "C2", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd","In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf",
+"Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Fl", "Lv"]
+
 class Syntaxfel(Exception):
 	pass
 
 def storeMolekyl(molekyl):
-	number = ''
+	"""Lägger in strängen i kön"""
 	for symbol in molekyl:
-		if symbol.isdigit():
-			q.enqueue(str(symbol))
-			print(symbol, "enqueueas")
-		else:
-			q.enqueue(symbol)
-			print(symbol, "enqueueas")
-	if number:
-		q.enqueue(number)
-		print(number, "enqueueas")
+		q.enqueue(symbol)
+	#q.enqueue('\n')
 	return q
 
 def readMolekyl():
-	readAtom()
-	if q.peek() == '':
+	"""<mol>   ::= <group> | <group><mol>"""
+	readGrupp()
+	if q.peek() == None:
+		return
+	if not q.peek() == ")":
+		try:
+			readMolekyl()
+		except Syntaxfel:
+			pass
+
+def readGrupp():
+	"""<group> ::= <atom> |<atom><num> | (<mol>) <num>"""
+	
+	if q.peek() == "(":
 		q.dequeue()
-	else:
+		readMolekyl()
+
+		if q.peek() == "\n":
+			raise Syntaxfel("Saknar högerparantes")
+
+		if q.peek() == ")":
+			q.dequeue()
+		
+			if q.peek().isdigit():
+				readNum()
+				return
+			raise Syntaxfel("Saknar siffra vid radslut")
+
+	readAtom()
+	if not q.isEmpty():
 		readNum()
+	
 
 def readAtom():
+	"""<atom>  ::= <LETTER> | <LETTER><letter>"""
 	readVersal()
 	if q.peek() is None or q.peek().isdigit():
 		return 
@@ -35,37 +59,37 @@ def readAtom():
 		readGemen() 
 
 def readVersal():
+	"""<LETTER>::= A | B | C | ... | Z"""
 	versal = q.dequeue()
-	#print(versal, "dequeueas i versal")
+	print(versal, "dequeueas i versal")
 	if versal.isupper():
 		return
-	raise Syntaxfel("Den är inte versal: " + versal)
+	raise Syntaxfel("Följer inte syntaxen, versal!")
 
 def readGemen():
+	"""<letter>::= a | b | c | ... | z"""
 	gemen = q.dequeue()
-	#print(gemen, "dequeueas i gemen")
+	print(gemen, "dequeueas i gemen")
 	if gemen.islower():
 		return
-	raise Syntaxfel("Det är inte en gemen: " + gemen)
+	raise Syntaxfel("Följer inte syntaxen, gemen!")
 
 def readNum():
-	num = ''
-	if q.isEmpty():
-		return
-	while not q.isEmpty():
-		print(q.peek(), "dequeueas som num")
-		if q.peek().isdigit():
-			num += q.dequeue()
-			print("nu är num: ", num)
+	"""<num>   ::= 2 | 3 | 4 | ..."""
+	try:
+		if int(q.peek()) >= 2:
+			print(q.peek(), "dequeueas i num")
+			q.dequeue()
+			
 		else:
-			raise Syntaxfel("Atomen måste sluta på en siffra: " + q.peek())
+			q.dequeue()
+			print("För litet tal vid radslut ")
+			sys.exit()
+	except (ValueError,TypeError):
+		raise Syntaxfel("Inte en siffra")
 
-	if int(num) >= 2:
-		return
-	else:
-		raise Syntaxfel("Siffran är för liten: " + num)
-
-def checksyntax(molekyl):
+def readFormel(molekyl):
+	"""<formel>::= <mol> \n"""
 	q = storeMolekyl(molekyl)
 	try:
 		readMolekyl()
@@ -77,7 +101,7 @@ def checksyntax(molekyl):
 def main():
 	
 	molekyl = input("skriv en molekyl: ")
-	resultat = checksyntax(molekyl)
+	resultat = readFormel(molekyl)
 	print(resultat)
 	main()
 
