441884abf31ad6e0eabc8489950bf41edd5ffc71
arvidlarzzon <arvidlarzzon@hotmail.com>
2017-04-25 20:05:09 +0200

    Lite fixande

--- AL-AN-Tilda/09/lab9.py
+++ AL-AN-Tilda/09/lab9.py
@@ -4,8 +4,8 @@ import string
 
 q = LinkedQ()
 
-ATOMER = ["H", "He", "Li", "Be", "B", "C2", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd","In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf",
-"Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Fl", "Lv"]
+ATOMER = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Fl', 'Lv']
+
 
 class Syntaxfel(Exception):
 	pass
@@ -14,7 +14,6 @@ def storeMolekyl(molekyl):
 	"""Lägger in strängen i kön"""
 	for symbol in molekyl:
 		q.enqueue(symbol)
-		print(symbol)
 	#q.enqueue('\n')
 	return q
 
@@ -23,7 +22,7 @@ def readMolekyl():
 	readGrupp()
 	if q.peek() == None:
 		return
-	if not q.peek() == ")":
+	if q.peek() != ")":
 		try:
 			readMolekyl()
 		except Syntaxfel:
@@ -31,9 +30,15 @@ def readMolekyl():
 
 def readGrupp():
 	"""<group> ::= <atom> |<atom><num> | (<mol>) <num>"""
-	readAtom()
+	print("inne i readGrupp")
+	if q.peek().isupper():
+		print("Storbokstav uppfylld skickar till readAtom")
+		readAtom()
+		if q.peek().isalpha():
+			readMolekyl()
 
 	if q.peek() == "(":
+		print(q.peek(), "hittad")
 		q.dequeue()
 		readMolekyl()
 
@@ -48,6 +53,9 @@ def readGrupp():
 				return
 			raise Syntaxfel("Saknar siffra vid radslut")
 
+	# HÄR MÅSTE VI KOLLA OM DET FINNS FLER ATOMER I GRUPPEN	
+
+
 	#readAtom()
 	if not q.isEmpty():
 		readNum()
@@ -56,25 +64,25 @@ def readGrupp():
 def readAtom():
 	"""<atom>  ::= <LETTER> | <LETTER><letter>"""
 	if q.peek().isalpha():
-		return
 	
-	if q.peek().isupper():
-		x = q.dequeue()
-		print(x, "I atom 1")
-	else:
-		raise Syntaxfel("Saknar stor bokstav i början")
+		if q.peek().isupper():
+			x = q.dequeue()
+			print(x, "I atom 1")
+		else:
+			raise Syntaxfel("Saknar stor bokstav i början")
 
-	if q.peek() is None:
-		return
+		if q.peek() is None:
+			return
 
-	if q.peek().islower():
-		x = x + q.dequeue()
-		print(x, "I atom 2")
+		if q.peek().islower():
+			x = x + q.dequeue()
+			print(x, "I atom 2")
 		
-	if x in ATOMER:
-		return
-	else:
-		raise Syntaxfel("Okänd atom")
+		if x in ATOMER:
+			print(x, "finns i ATOMER")
+			return
+		else:
+			raise Syntaxfel("Okänd atom")
 
 def readNum():
 	"""<num>   ::= 2 | 3 | 4 | ..."""
@@ -110,3 +118,5 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+#Si(C3(COOH)2)4(H2O)7
